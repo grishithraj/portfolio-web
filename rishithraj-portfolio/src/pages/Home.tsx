c11a8b72a368da4fd223d261432ac69f721f688c
grishithraj <rishithefx@gmail.com>
2025-07-07 14:10:26 +0530

    added smooth cursor

--- rishithraj-portfolio/src/pages/Home.tsx
+++ rishithraj-portfolio/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import NavbarDemo from "../components/navbar";
 import Hero from "../components/hero";
 import Projects from "../components/project";
 import Contact from "../components/contact";
+import SmoothCursorDemo from "../components/SmoothCursorDemo";
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
         <Hero />
         <Projects />
         <Contact />
+        <SmoothCursorDemo />
       </div>
     </>
   );
